a04ad1544de01e3713aa5b4ab8e99e266a52fb5b
Omar <60241338+OmarChefrad@users.noreply.github.com>
2023-12-18 23:05:52 +0100

    useradd chmod

---
+++ addUser.sh
@@ -0,0 +1,17 @@
+useradd –u 2040 –g DepInfo t –G s1 –
+e 2016-07-01 –s /bin/bash x –d /home/newUser newUser
+
+sudo usermod -c an "c'est mon compte" -s /bin/bash newUser
+
+sudo usermod –a –G sudo user1
+
+-rwx rw- r--
+chmod u+x fichier
+chmod g-w fichier
+chmod o+rx fichier
+chmod u=r-x fichier
+
+- rwx rw- r-- 
+chgrp
+
+
