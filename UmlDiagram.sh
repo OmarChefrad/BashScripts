313830607ef5e92bbfbfec67ea4bc74cc311487e
Omar <60241338+OmarChefrad@users.noreply.github.com>
2023-12-26 20:27:39 +0100

    Create UmlDiagram.sh

---
+++ UmlDiagram.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# UML Class Diagram for Person
+
+echo "-----------------------------------"
+echo "|             Person              |"
+echo "|---------------------------------|"
+echo "| - name: String                  |"
+echo "| - age: int                      |"
+echo "|---------------------------------|"
+echo "| + getName(): String             |"
+echo "| + getAge(): int                 |"
+echo "| + setName(name: String): void   |"
+echo "| + setAge(age: int): void        |"
+echo "| + setSalary(salary: int): void  |"
+echo "-----------------------------------"
